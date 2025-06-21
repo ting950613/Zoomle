9dad52c533d921b9997e30b56d1c9e0c4f892000
ting950613 <126735419+ting950613@users.noreply.github.com>
2025-06-20 22:55:59 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Zoomle from './games/Zoomle';
-import Emovi from './games/Emovi';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/zoomle" element={<Zoomle />} />
-        <Route path="/emovi" element={<Emovi />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
