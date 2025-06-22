4a11bacd64cf7241f82fe069e97c7a3f0289bb20
ting950613 <126735419+ting950613@users.noreply.github.com>
2025-06-22 00:52:09 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Logo } from './components/Logo'; // Your logo component
+import Logo from './components/Logo';
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(false);
